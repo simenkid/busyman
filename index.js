64502acb2f60e679ea221310a4507d493f3d99c6
Simen Li <simenkid@gmail.com>
2016-07-27 21:14:43 +0800

    Object.assign polyfill

--- index.js
+++ index.js
@@ -75,6 +75,29 @@ _.isPlainObject = function (val) {
 /*************************************************************************************************/
 /*** Object                                                                                    ***/
 /*************************************************************************************************/
+// polyfill for ES5
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+
 _.assign = Object.assign;
 
 _.keys = Object.keys;
